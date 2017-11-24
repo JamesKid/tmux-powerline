97084104ffecfacb75f330259798a042aa7f2367
vimkidgit <1779156598@qq.com>
2017-11-24 16:03:31 +0800

    fix who in my server

--- segments/who.sh
+++ segments/who.sh
@@ -7,10 +7,10 @@
 
 # check who in server
 	run_segment() {
-    mkdir -p /tmp/status/
-		cd /tmp/status/
-		rm -rf /tmp/status/who.txt
-		wget http://www.vimkid/assets/file/who.txt 
-		mysta=`cat /tmp/status/who.txt`
-		echo $mysta
+      mkdir -p /tmp/status/
+      cd /tmp/status/
+      rm -rf /tmp/status/who.*
+      wget http://www.vimkid.com/assets/who.txt 
+      mysta=`cat /tmp/status/who.txt`
+      echo $mysta
 	}
